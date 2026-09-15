774f7d70cb9f9abb6c949feb8ccf95d132564a2c
Kamallochan Panigrahi <143154588+Kamalproject@users.noreply.github.com>
2026-09-15 20:17:30 +0530

    Disable the button until you click on the check box

--- java.js
+++ java.js
@@ -1,12 +1,7 @@
-function validateForm(){
-    var pass=document.getElementById("pass").value;
-    var email=document.getElementById("email").value;
-    var error=document.getElementById("error");
-    if(!email.includes('@')){
-        error.innerHTML="Email must contain @";
-        
-    }
-    else if(pass.length<6){
-        error.innerHTML="Password length should be greater then 6";
-    }
+function clicktounable(){
+    var text=document.getElementById("text");
+
+    var btn=document.getElementById("btn");
+
+    btn.disabled=!text.checked;
 }
